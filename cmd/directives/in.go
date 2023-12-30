35d7993c68242a3a1031550e89d70e0455c1c2d3
Nick Palenchar <nickpal@nickpalenchar.com>
2023-12-30 15:45:14 -0500

    use users shel

--- cmd/directives/in.go
+++ cmd/directives/in.go
@@ -4,14 +4,24 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 // In executes each shell command in the specified directory.
 func In(directory string, commands []string) error {
 
+	if trimmed := strings.TrimSpace(directory); trimmed == "" {
+		log.Fatalf("[ERROR] Directive IN must have a path (e.g. `IN .`; `IN /home`)")
+	}
+
+	shell := os.Getenv("SHELL")
+	if shell == "" {
+		shell = "sh"
+	}
+
 	// Iterate over each command and execute it
 	for _, cmdStr := range commands {
-		cmd := exec.Command("sh", "-c", cmdStr)
+		cmd := exec.Command(shell, "-c", cmdStr)
 		cmd.Dir = directory
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
--- cmd/directives/main.go
+++ cmd/directives/main.go
@@ -1,7 +1,1 @@
 package directives
-
-import "fmt"
-
-func main() {
-	fmt.Println("Hello from MyFunction in mypackage!")
-}
